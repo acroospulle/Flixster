4b54fa03a2a3939870ab476344a66d3d2de647f5
acroospulle <Ashni.Croospulle@cix.csi.cuny.edu>
2022-01-04 13:59:22 -0500

    datasource and delegate implemented

--- Flixster/MovieGridViewController.swift
+++ Flixster/MovieGridViewController.swift
@@ -7,13 +7,20 @@
 
 import UIKit
 
-class MovieGridViewController: UIViewController {
+class MovieGridViewController: UIViewController,UICollectionViewDataSource,UICollectionViewDelegate {
+    
+    
 
+
+    @IBOutlet weak var collectionView: UICollectionView!
     var movies = [[String:Any]]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
         let url = URL(string: "https://api.themoviedb.org/3/movie/297762/similar?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
@@ -31,7 +38,14 @@ class MovieGridViewController: UIViewController {
         
     }
     
-
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        movies.count
+    }
     
-
+   
+   
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+    }
 }
